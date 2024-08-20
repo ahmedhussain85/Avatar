9bdb11c9ac959874e1ea1d21e1335b7a023a5373
Pedro Stradioto <pedrostradioto@gmail.com>
2024-08-20 15:05:56 +0200

    Add Swagger code to the Documentation Header

--- src/main/java/SpringBootSwaggerIntegrator/com/example/SpringSwaggerIntegration/Application.java
+++ src/main/java/SpringBootSwaggerIntegrator/com/example/SpringSwaggerIntegration/Application.java
@@ -2,11 +2,18 @@ package SpringBootSwaggerIntegrator.com.example.SpringSwaggerIntegration;
 
 import java.util.Arrays;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.tags.Tag;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+
 
 
 /*
@@ -24,7 +31,22 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-
+	//<---This is API Related(Swagger)--->
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+				.info(new Info()
+						.title("Custom API")
+						.description("This is a custom API description")
+						.version("1.0")
+						.contact(new Contact().name("API Support").email("support@example.com"))
+						.license(new License().name("Apache 2.0").url("http://springdoc.org")))
+				.externalDocs(new ExternalDocumentation()
+						.description("External Documentation")
+						.url("https://example.com")) ;
+		//.addTagsItem(new Tag().name("custom-tag").description("This is a custom tag"));
+	}
+	//<---This is API Related(Swagger)--->
 
 	@Bean
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
@@ -40,6 +62,8 @@ public class Application {
 
 		};
 	}
+
+
 
 }
 
